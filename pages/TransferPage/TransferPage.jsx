75f489d8016cee0acff8a1adf5f6b2054f4c58e3
Martin Rodriguez <martin.rodriguez@mcplusa.com>
2023-09-22 20:52:10 -0300

    Modifique la pagina para afectar a los datos reales de las cuentas

--- pages/TransferPage/TransferPage.jsx
+++ pages/TransferPage/TransferPage.jsx
@@ -3,6 +3,15 @@ import Header from "@/components/Header/Header";
 import NavBar from "@/components/NavBar/NavBar";
 import { Transferencias } from "@/components/Transferencias/Transferencias";
 
+
+export async function getServerSideProps(context) {
+
+    return {
+        props: {
+            /* Aca hay que usar los props con el valor de los inputs */
+        }
+    }
+}
 export default function TransferPage() {
     return (
         <>
