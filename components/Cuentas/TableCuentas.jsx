c7ae7113dfa5c1f650c0a6830b7574e7b8e134e5
Martin Rodriguez <martin.rodriguez@mcplusa.com>
2023-09-23 17:03:43 -0300

    ahora pasamos la cuentas como prop

--- components/Cuentas/TableCuentas.jsx
+++ components/Cuentas/TableCuentas.jsx
@@ -1,5 +1,9 @@
+import Link from 'next/link'
+import { getAccounts } from '@/data/accounts';
 
 export const TableCuentas = ({ accounts }) => {
+
+
     return (
         <table className="table table-hover" id="table">
             <thead className="table-dark">
@@ -11,31 +15,15 @@ export const TableCuentas = ({ accounts }) => {
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>123456789</td>
-                    <td>John Doe</td>
-                    <td>$10000</td>
-                    <td>Cuenta de Ahorros</td>
-                </tr>
-                <tr>
-                    <td>987654321</td>
-                    <td>Jane Smith</td>
-                    <td>$5500</td>
-                    <td>Cuenta Corriente</td>
-                </tr>
-                <tr>
-                    <td>555555555</td>
-                    <td>Michael Johnson</td>
-                    <td>$2300</td>
-                    <td>Cuenta de Ahorros</td>
-                </tr>
                 {accounts.map((account, index) => (
-                    <tr key={index}>
-                        <td>{account.number}</td>
-                        <td>{account.name}</td>
-                        <td>{account.balance}</td>
-                        <td>{account.type}</td>
-                    </tr>
+                    <Link href={`/cuentas/${account.numeroCuenta}`} key={index}>
+                        <tr>
+                            <td>{account.numeroCuenta}</td>
+                            <td>{account.titularCuenta}</td>
+                            <td>{account.saldo}</td>
+                            <td>{account.tipoCuenta}</td>
+                        </tr>
+                    </Link>
                 ))}
             </tbody>
         </table>
